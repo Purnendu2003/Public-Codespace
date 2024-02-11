4bd6d3a57322043ddcc55865826db8ac14af75e9
Purnendu Bhattacharyya <159695437+Purnendu2003@users.noreply.github.com>
2024-02-11 17:28:47 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout << "Enter Two Numbers";
+    cin >> a;
+    cin >> b;
+
+    c = a + b;
+
+    cout << "The Result is " << c << "\n";
+
+    return 0;
+}
